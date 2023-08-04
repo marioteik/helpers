2287ef28369ac23075d5c89bd34c6ac81bf2a3d1
marioteik <mario.teik@gmail.com>
2023-08-04 08:53:18 -0300

    add new identity changes

--- helpers.go
+++ helpers.go
@@ -1,6 +1,8 @@
 package helpers
 
 import (
+	"crypto/rand"
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"io"
@@ -78,6 +80,38 @@ func ValidateStruct(data any) error {
 
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func SQLToNullString(s string) sql.NullString {
+	if s != "" {
+		return sql.NullString{
+			String: s,
+			Valid:  true,
+		}
+	}
+	return sql.NullString{}
+}
+
+func EncodeToString(max int) string {
+	b := make([]byte, max)
+	n, err := io.ReadAtLeast(rand.Reader, b, max)
+	if n != max {
+		panic(err)
+	}
+	for i := 0; i < len(b); i++ {
+		b[i] = table[int(b[i])%len(table)]
+	}
+	return string(b)
+}
+
+var table = [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+
+func CompareCode(hashedCode, plainText string) error {
+	if hashedCode != plainText {
+		return errors.New("invalid code")
 	}
 
 	return nil
